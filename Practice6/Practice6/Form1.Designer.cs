60a05df9a4fcd7a0c5cc4ace7f592d9e67e527b6
Вера Воробьева <lapineda@list.ru>
2021-04-14 19:21:10 +0300

    Add 5 and 6 practices

---
+++ Practice6/Practice6/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Practice6
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.textForCreate = new System.Windows.Forms.TextBox();
+      this.textForFind = new System.Windows.Forms.TextBox();
+      this.buttonForCreate = new System.Windows.Forms.Button();
+      this.buttonForFind = new System.Windows.Forms.Button();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // textForCreate
+      // 
+      this.textForCreate.Location = new System.Drawing.Point(82, 26);
+      this.textForCreate.Multiline = true;
+      this.textForCreate.Name = "textForCreate";
+      this.textForCreate.Size = new System.Drawing.Size(212, 260);
+      this.textForCreate.TabIndex = 0;
+      // 
+      // textForFind
+      // 
+      this.textForFind.Location = new System.Drawing.Point(421, 26);
+      this.textForFind.Multiline = true;
+      this.textForFind.Name = "textForFind";
+      this.textForFind.Size = new System.Drawing.Size(212, 260);
+      this.textForFind.TabIndex = 1;
+      // 
+      // buttonForCreate
+      // 
+      this.buttonForCreate.Location = new System.Drawing.Point(135, 311);
+      this.buttonForCreate.Name = "buttonForCreate";
+      this.buttonForCreate.Size = new System.Drawing.Size(104, 26);
+      this.buttonForCreate.TabIndex = 2;
+      this.buttonForCreate.Text = "Заполнить";
+      this.buttonForCreate.UseVisualStyleBackColor = true;
+      this.buttonForCreate.Click += new System.EventHandler(this.buttonForCreate_Click);
+      // 
+      // buttonForFind
+      // 
+      this.buttonForFind.Location = new System.Drawing.Point(488, 311);
+      this.buttonForFind.Name = "buttonForFind";
+      this.buttonForFind.Size = new System.Drawing.Size(104, 26);
+      this.buttonForFind.TabIndex = 3;
+      this.buttonForFind.Text = "Поиск";
+      this.buttonForFind.UseVisualStyleBackColor = true;
+      this.buttonForFind.Click += new System.EventHandler(this.buttonForFind_Click);
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(132, 10);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(99, 13);
+      this.label1.TabIndex = 4;
+      this.label1.Text = "Исходный массив";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(473, 10);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(119, 13);
+      this.label2.TabIndex = 5;
+      this.label2.Text = "Найденные элементы";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(707, 362);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.buttonForFind);
+      this.Controls.Add(this.buttonForCreate);
+      this.Controls.Add(this.textForFind);
+      this.Controls.Add(this.textForCreate);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TextBox textForCreate;
+    private System.Windows.Forms.TextBox textForFind;
+    private System.Windows.Forms.Button buttonForCreate;
+    private System.Windows.Forms.Button buttonForFind;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+  }
+}
+
---
+++ Practice6/Practice6/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practice6
+{
+  public partial class Form1 : Form
+  {
+    int[] mas = new int[15];
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void buttonForCreate_Click(object sender, EventArgs e)
+    {
+      CreateArray();
+    }
+    private void buttonForFind_Click(object sender, EventArgs e)
+    {
+      Find();
+    }
+    void CreateArray()
+    {
+      Random rnd = new Random();
+      textForCreate.Text = "";
+      for(int i = 0; i < 15; i++)
+      {
+        mas[i] = rnd.Next(-50, 50);
+        textForCreate.Text += "Mas[" + i + "] = " + Convert.ToString(mas[i]) + Environment.NewLine;
+      }
+    }
+
+    public void Find()
+    {
+      textForFind.Text = "";
+      for (int i = 0; i < 15; i++)
+      {
+        if (mas[i] > 0 && mas[i] < 8) textForFind.Text += "Mas[" + i + "] = " + Convert.ToString(mas[i]) + Environment.NewLine;
+      }
+      string Check = textForFind.Text;
+      if (Check == "") textForFind.Text = "Таких чисел нет";
+    }
+   
+  }
+}
